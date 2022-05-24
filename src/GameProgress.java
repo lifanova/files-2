90c549005ae1e48ce9bc6fe3ef9758cfb4ab1968
vlifanova <vlifanova2@alfabank.ru>
2022-05-24 15:23:00 +0300

    GameProgress & Main

---
+++ src/GameProgress.java
@@ -0,0 +1,64 @@
+import java.io.Serializable;
+
+/**
+ * Created by Vera Lifanova
+ * Date: 24.05.2022
+ */
+
+public class GameProgress implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int health;
+    private int weapons;
+    private int lvl;
+    private double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(int weapons) {
+        this.weapons = weapons;
+    }
+
+    public int getLvl() {
+        return lvl;
+    }
+
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" +
+                "health=" + health +
+                ", weapons=" + weapons +
+                ", lvl=" + lvl +
+                ", distance=" + distance +
+                '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Vera Lifanova
+ * Date: 24.05.2022
+ */
+
+public class Main {
+}
