5d07207d46a9f5bf8209f1096008a008175d046f
vlifanova <vlifanova2@alfabank.ru>
2022-05-25 09:08:09 +0300

    GameProgress & Main

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,23 @@ import java.util.zip.ZipOutputStream;
  */
 
 public class Main {
+    public static final String PATH = "C://Games/savegames/";
+
     public static void main(String[] args) {
         GameProgress firstGame = new GameProgress(1, 1, 1, 27);
         GameProgress secondGame = new GameProgress(2, 6, 2, 63);
         GameProgress thirdGame = new GameProgress(3, 5, 3, 45);
 
         List<String> files = new ArrayList<>();
-        files.add("C://Games/savegames/save1.dat");
-        files.add("C://Games/savegames/save2.dat");
-        files.add("C://Games/savegames/save3.dat");
+        files.add("save1.dat");
+        files.add("save2.dat");
+        files.add("save3.dat");
 
-        saveGame("C://Games/savegames/save1.dat", firstGame);
-        saveGame("C://Games/savegames/save2.dat", secondGame);
-        saveGame("C://Games/savegames/save3.dat", thirdGame);
+        saveGame(PATH + "save1.dat", firstGame);
+        saveGame(PATH + "save2.dat", secondGame);
+        saveGame(PATH + "save3.dat", thirdGame);
 
-        zipFiles("C://Games/savegames/all.zip", files);
+        zipFiles(PATH + "all.zip", files);
     }
 
     private static void saveGame(String path, GameProgress gameProgress) {
@@ -39,26 +41,30 @@ public class Main {
     }
 
     private static void zipFiles(String zipPath, List<String> files) {
-        for (String path : files) {
-            processFile(zipPath, path);
-        }
 
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipPath))) {
+            for (String filename : files) {
+                processFile(zout, filename);
+                // закрываем текущую запись для новой записи
+                zout.closeEntry();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private static void processFile(String zipPath, String path){
-        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(zipPath));
-             FileInputStream fis = new FileInputStream(path)) {
-            ZipEntry entry = new ZipEntry(path);
+    private static void processFile(ZipOutputStream zout, String filename) {
+        String filepath = PATH + filename;
+        try (FileInputStream fis = new FileInputStream(filepath)) {
+            ZipEntry entry = new ZipEntry(filename);
             zout.putNextEntry(entry);
             // считываем содержимое файла в массив byte
             byte[] buffer = new byte[fis.available()];
             fis.read(buffer);
             // добавляем содержимое к архиву
             zout.write(buffer);
-            // закрываем текущую запись для новой записи
-            zout.closeEntry();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
